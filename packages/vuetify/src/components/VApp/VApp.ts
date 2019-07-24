52f18bd73c5849903a49896a37d2afb14f9ea2d3
Jacek Karczmarczyk <jk@drukcd.pl>
2019-07-24 20:16:39 +0200

    chore: throw if vuetify is not bootstrapped properly

--- packages/vuetify/src/components/VApp/VApp.ts
+++ packages/vuetify/src/components/VApp/VApp.ts
@@ -34,6 +34,12 @@ export default mixins(
     },
   },
 
+  beforeCreate () {
+    if (process.env.NODE_ENV === 'development' && (!this.$vuetify || this.$vuetify === this.$root)) {
+      throw new Error('Vuetify is not properly initialized, see https://vuetifyjs.com/en/getting-started/quick-start#bootstrapping-the-vuetify-object')
+    }
+  },
+
   render (h) {
     const wrapper = h('div', { staticClass: 'v-application--wrap' }, this.$slots.default)
 
